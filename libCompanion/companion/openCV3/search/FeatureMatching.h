8996e4c5705b75152262a4ead6d3e5d492a2f9f5
Andreas Sekulski <andreas.sekulski@gmail.com>
2017-01-03 17:31:53 +0100

    XFeatures2D.hpp removed.

--- libCompanion/companion/openCV3/search/FeatureMatching.h
+++ libCompanion/companion/openCV3/search/FeatureMatching.h
@@ -7,7 +7,6 @@
 
 #include "ImageRecognition.h"
 #include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/xfeatures2d.hpp>
 
 /**
  * Feature matching algo implementation from <a href="http://docs.opencv.org/3.1.0/d5/d6f/tutorial_feature_flann_matcher.html">OpenCV</a>.
