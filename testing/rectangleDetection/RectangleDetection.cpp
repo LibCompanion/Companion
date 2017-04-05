25ce908fc685750f87412dee3c5afd33b759a676
Andreas Sekulski <andreas.sekulski@gmail.com>
2017-04-05 13:55:12 +0200

    RectangleDetection testing changed.

--- testing/rectangleDetection/RectangleDetection.cpp
+++ testing/rectangleDetection/RectangleDetection.cpp
@@ -1,192 +1,21 @@
-//
-// Created by asekulsk on 11.01.17.
-//
-
-#include "RectangleDetection.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
+#include <math.h>
+#include <string.h>
 
-cv::Mat src; cv::Mat src_gray;
-int thresh = 100;
-int max_thresh = 255;
-cv::RNG rng(12345);
-cv::Mat dst, detected_edges;
+using namespace cv;
+using namespace std;
 
-RectangleDetection::RectangleDetection() {}
+int thresh = 50, N = 5;
+const char* wndname = "Square Detection Demo";
 
-RectangleDetection::~RectangleDetection() {
-
-}
-
-void variant_one() {
-    // http://docs.opencv.org/2.4/doc/tutorials/imgproc/shapedescriptors/bounding_rects_circles/bounding_rects_circles.html
-
-    std::vector<std::vector<cv::Point> > contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::Mat threshold_output;
-
-    /// Reduce noise with a kernel 3x3
-    blur(src_gray, src_gray, cv::Size(3,3));
-
-    /// Detect edges using Threshold
-    Canny(src_gray, threshold_output, 10, thresh);
-    //threshold(src_gray, threshold_output, thresh, 255, cv::THRESH_BINARY); // Creates an radius
-
-    /// Find contours
-    findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
-
-    /// Approximate contours to polygons + get bounding rects
-    std::vector<std::vector<cv::Point>> contours_poly(contours.size());
-    std::vector<cv::Rect> boundRect;
-    cv::Rect bound, compareBound;
-
-    for(int i = 0; i < contours.size(); i++ ) {
-        approxPolyDP(cv::Mat(contours[i]), contours_poly[i], 3, true );
-        bound = boundingRect(cv::Mat(contours_poly[i]));
-
-        // Don't use small areas from bounding rects
-        if(bound.area() > 1000) {
-            boundRect.push_back(bound);
-        }
-    }
-
-    // ToDo := Implement CA-Algo (Cross Area)
-    // ToDo := Nice name ;-)
-    unsigned long z = 0, x = 0;
-
-    while (z < boundRect.size()) {
-
-        bound = boundRect.at(z);
-        compareBound = boundRect.at(x);
-
-        // Check if rectangles overlapping
-        // http://answers.opencv.org/question/67091/how-to-find-if-2-rectangles-are-overlapping-each-other/
-        if(z != x && (compareBound & bound).area() > 0) {
-            // Erase rectangles
-            boundRect.erase (boundRect.begin() + z);
-            boundRect.erase (boundRect.begin() + x - 1);
-
-            // Union rectangles
-            boundRect.push_back((compareBound | bound));
-
-            z = 0; // Begin comparison on the scratch
-            x = 0; // Begin comparison on the scratch
-        } else {
-            x++;
-        }
-
-        if(x == boundRect.size()) {
-            z++;
-            x = 0;
-        }
-    }
-
-    /// Draw contours
-    cv::Scalar color;
-    cv::Mat drawing = cv::Mat::zeros(threshold_output.size(), CV_8UC3 );
-    for(int i = 0; i< contours.size(); i++ ) {
-        color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, cv::Point());
-    }
-
-    cv::Mat result;
-    src.copyTo(result);
-    /// Draw bonding rects
-    for(unsigned long i = 0; i< boundRect.size(); i++ ) {
-        color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-        bound = boundRect.at(i);
-        rectangle(result, bound.tl(), bound.br(), color, 2, 8, 0 );
-    }
-
-    /// Show in a window
-    cv::namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-    imshow("Contours", drawing);
-    imshow("Result", result);
-}
-
-void variant_two() {
-    // https://pythontips.com/2015/03/11/a-guide-to-finding-books-in-images-using-python-and-opencv/
-
-    cv::Mat temp;
-    src_gray.copyTo(temp);
-
-    /// Reduce noise with a kernel 3x3
-    blur(temp, temp, cv::Size(3,3));
-    cv::imshow("Blur", temp);
-
-    /// Canny detector
-    Canny(temp, detected_edges, 10, thresh);
-    cv::imshow("Edged", detected_edges);
-
-    cv::Mat closed;
-    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
-    morphologyEx(detected_edges, closed, cv::MORPH_CLOSE, kernel);
-    cv:imshow("Closed", closed);
-
-    std::vector<std::vector<cv::Point> > contours;
-    std::vector<cv::Point> contours_poly( contours.size() );
-    cv::findContours(closed, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-    cv::Mat copy;
-    src.copyTo(copy);
-
-    for(size_t i = 0; i< contours.size(); i++ ) {
-        double peri = cv::arcLength(contours[i], true);
-        cv::approxPolyDP(contours[i], contours_poly, 0.02 * peri, true);
-
-        if(contours_poly.size() == 4) {
-            cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
-            cv::drawContours(copy, contours, -1, color, 4);
-        }
-    }
-
-    cv::imshow("Result", copy);
-}
-
-void RectangleDetection::thresh_callback(int, void *) {
-    variant_one();
-    //variant_two();
-}
-
-void RectangleDetection::detect(cv::Mat img) {
-
-    src = img;
-
-    /// Convert the image to grayscale
-    cvtColor(src, src_gray, CV_BGR2GRAY );
-
-    /// Create Window
-    char* source_window = "Source";
-    cv::namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-    imshow(source_window, src);
-    /// Create a Trackbar for user to enter threshold
-    cv::createTrackbar("Threshold:", source_window, &thresh, max_thresh, thresh_callback );
-
-    /// Show the image
-    thresh_callback(0, 0);
-}
-/*
-int main( int argc, char** argv )
-{
-
-    RectangleDetection *rtc = new RectangleDetection();
-    rtc->detect(cv::imread("/home/asekulsk/Dokumente/Master/Testcase/HBF/Sample_Left.jpg"));
-
-    return 0;
-}
- */
-
-/**
- * Simple shape detector program.
- * It loads an image and tries to find simple shapes (rectangle, triangle, circle, etc) in it.
- * This program is a modified version of `squares.cpp` found in the OpenCV sample dir.
- */
-
-/**
- * Helper function to find a cosine of angle between vectors
- * from pt0->pt1 and pt0->pt2
- */
-static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+static double angle( Point pt1, Point pt2, Point pt0 )
 {
     double dx1 = pt1.x - pt0.x;
     double dy1 = pt1.y - pt0.y;
@@ -195,85 +24,134 @@ static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
     return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
 
-/**
- * Helper function to display text in the center of a contour
- */
-void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
+// returns sequence of squares detected on the image.
+// the sequence is stored in the specified memory storage
+static void findSquares( const Mat& image, vector<vector<Point> >& squares )
 {
-    int fontface = cv::FONT_HERSHEY_SIMPLEX;
-    double scale = 0.4;
-    int thickness = 1;
-    int baseline = 0;
+    squares.clear();
 
-    cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
-    cv::Rect r = cv::boundingRect(contour);
+    //Mat pyr, timg, gray0(image.size(), CV_8U), gray;
+    // down-scale and upscale the image to filter out the noise
+    //pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
+    //pyrUp(pyr, timg, image.size());
 
-    cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
-    cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
-    cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+    // blur will enhance edge detection
+    Mat timg(image);
+    medianBlur(image, timg, 9);
+    Mat gray0(timg.size(), CV_8U), gray;
+
+    vector<vector<Point> > contours;
+
+    // find squares in every color plane of the image
+    for( int c = 0; c < 3; c++ )
+    {
+        int ch[] = {c, 0};
+        mixChannels(&timg, 1, &gray0, 1, ch, 1);
+
+        // try several threshold levels
+        for( int l = 0; l < N; l++ )
+        {
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if( l == 0 )
+            {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+                Canny(gray0, gray, 5, thresh, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                dilate(gray, gray, Mat(), Point(-1,-1));
+            }
+            else
+            {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l+1)*255/N;
+            }
+
+            // find contours and store them all as a list
+            findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+            vector<Point> approx;
+
+            // test each contour
+            for( size_t i = 0; i < contours.size(); i++ )
+            {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                if( approx.size() == 4 &&
+                    fabs(contourArea(Mat(approx))) > 1000 &&
+                    isContourConvex(Mat(approx)) )
+                {
+                    double maxCosine = 0;
+
+                    for( int j = 2; j < 5; j++ )
+                    {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if( maxCosine < 0.3 )
+                        squares.push_back(approx);
+                }
+            }
+        }
+    }
+}
+
+// the function draws all the squares in the image
+static void drawSquares( Mat& image, const vector<vector<Point> >& squares )
+{
+    for( size_t i = 0; i < squares.size(); i++ )
+    {
+        const Point* p = &squares[i][0];
+
+        int n = (int)squares[i].size();
+        //dont detect the border
+        if (p-> x > 3 && p->y > 3)
+            polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, LINE_AA);
+    }
+
+    imshow(wndname, image);
 }
 
 int main()
 {
-    //cv::Mat src = cv::imread("polygon.png");
-    cv::Mat src = cv::imread("/home/asekulsk/Downloads/imgFull.jpg");
-    if (src.empty())
-        return -1;
+    // Souce code from -> https://github.com/alyssaq/opencv
+    cv::Mat frame;
+    cv::VideoCapture cap("/home/asekulsk/Dokumente/Master/Testcase/HBF/Muelheim_HBF.mp4");
 
-    // Convert to grayscale
-    cv::Mat gray;
-    cv::cvtColor(src, gray, CV_BGR2GRAY);
+    if (cap.isOpened()) {
 
-    // Use Canny instead of threshold to catch squares with gradient shading
-    cv::Mat bw;
-    cv::Canny(gray, bw, 3000, 1000, 5);
+        cap >> frame;
+        namedWindow( wndname, 1 );
+        vector<vector<Point> > squares;
 
-    cv::imshow("src_bw", bw);
+        while (!frame.empty()) {
 
-    // Find contours
-    std::vector<std::vector<cv::Point> > contours;
-    cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+            squares.clear();
 
-    std::vector<cv::Point> approx;
-    cv::Mat dst = src.clone();
-
-    for (int i = 0; i < contours.size(); i++)
-    {
-        // Approximate contour with accuracy proportional
-        // to the contour perimeter
-        cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
-
-        // Skip small or non-convex objects
-        if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
-            continue;
-
-        if (approx.size() >= 4 && approx.size() <= 6)
-        {
-            // Number of vertices of polygonal curve
-            int vtc = approx.size();
-
-            // Get the cosines of all corners
-            std::vector<double> cos;
-            for (int j = 2; j < vtc+1; j++)
-                cos.push_back(angle(approx[j%vtc], approx[j-2], approx[j-1]));
-
-            // Sort ascending the cosine values
-            std::sort(cos.begin(), cos.end());
-
-            // Get the lowest and the highest cosine
-            double mincos = cos.front();
-            double maxcos = cos.back();
-
-            // Use the degrees obtained above and the number of vertices
-            // to determine the shape of the contour
-            if (vtc == 4 && mincos >= -0.1 && maxcos <= 0.3) {
-                setLabel(dst, "RECT", contours[i]);
+            if(!frame.empty()) {
+                findSquares(frame, squares);
+                drawSquares(frame, squares);
+                waitKey(1);
             }
+
+            cap >> frame;
         }
     }
 
-    cv::imshow("src", src);
-    cv::imshow("dst", dst);
-    cv::waitKey(0);
     return 0;
 }
